7bb6ed03cf9792ce837a7e069acb068733a451a1
Abiodun <aabiodunawoyemi@gmail.com>
2023-11-06 12:52:08 +0100

    frontend code updated

--- frontend/src/components/core/Modal.tsx
+++ frontend/src/components/core/Modal.tsx
@@ -4,14 +4,14 @@ import Button from './Button'
 
 const Modal = ({ address, result, showModal, setShowModal }: IModal) => {
     console.log("resultresult: ", result);
-    
+
     return (
         <Fragment>
             {showModal && (
                 <div className="p-5 justify-center items-center flex overflow-x-hidden overflow-y-auto bg-[#5a99afa2] fixed inset-0 z-50 outline-none focus:outline-none">
                     <div className="relative mx-auto md:max-w-[500px]">
                         <div className="rounded-[30px] px-6 py-10 w-full font-mono shadow-lg relative bg-[#FFFFFF] outline-none focus:outline-none">
-                            This address <span className="md:break-words">{address ?? "0x0"}</span> is a type of {result?.type === "EOA" ? (<b>Externally Owned Account</b>) : result?.type === "contract" ? (<b>Contract Account</b>) : (<b>No type for this address!</b>)}
+                            This address <span className="md:break-words">{address ?? "0x0"}</span> is a type of {result?.type === "EOA" ? (<b>Externally Owned Account</b>) : result?.type === "contract" ? (<b>Contract Account</b>) : (<b>an invalid address!</b>)}
 
                             <div className="flex justify-end mt-16">
                                 <Button
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     colors: {
@@ -30,3 +29,4 @@ module.exports = {
   },
   plugins: [],
 }
+
